25da6c84bb5d1dc9ff74bd5c853725633e4ac6fc
mugisha isaie <mugishaisaieog1@gmail.com>
2025-09-26 07:47:53 +0200

    securing the person properties by adding validating

--- part3/model/person.js
+++ part3/model/person.js
@@ -14,8 +14,24 @@ mongoose.connect(url).then((result)=>{
 
 
 const personSchema = new mongoose.Schema({
-    name:String,
-    number: String
+    name:{
+        type:String,
+        minLength: 3,
+        required: true,
+        unique: true
+    },
+    number: {
+        type:String,
+        minLength: 8,
+        required: true,
+        validate:{
+            validator: function(v){
+                return/^\d{2,4}-\d+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+
+        }
+    }
 })
 
 personSchema.set('toJSON',{
