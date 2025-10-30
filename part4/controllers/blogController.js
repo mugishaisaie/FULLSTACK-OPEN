fdcfb1ce3f6689a1a7208f163acb23af67e7a290
Mugisha Isaie <mugishaisaieog1@gmail.com>
2025-10-30 14:30:41 +0200

    updating the website for getting data and creating data

--- part4/controllers/blogController.js
+++ part4/controllers/blogController.js
@@ -3,27 +3,23 @@ const Blog = require('../models/blogs');
 
 
 const listBlogs = (req,res,next)=>{
-    try {
+   
         Blog.find({}).then((blogs) => {
     res.json(blogs)
-  })
-    } catch (er ror) {
-        next(error)
+  }).catch(next)
+    
         
-    }
 }
 
 const createBlogs = (req,res,next)=>{
-    try{
+    
         const blog = new Blog(req.body)
 
   blog.save().then((result) => {
     res.status(201).json(result)
-  })
+  }).catch(next)
 
-    }catch(error){
-        next(error)
-    }
+   
 }
 
 module.exports ={
--- part4/models/blogs.js
+++ part4/models/blogs.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 
-const blogSchema = mongoose.Schema({
+const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
